f224a37a01db0ea2fbc0046df9d6d7307849ad69
akshatgg <akshatg9636@gmail.com>
2024-01-23 00:05:43 +0545

    edit method applied

--- crud/controller/usercontroller.js
+++ crud/controller/usercontroller.js
@@ -15,7 +15,7 @@ if(!name || !email){
     throw new Error("Name and email are required")
 }
  
-const userexists =await User.findOne({email})
+const userexists =await User.findOne({})
 
 if(userexists){
     throw new Error("User already exists")
@@ -41,4 +41,96 @@ res.status(400).json({
 }
 
 
+}
+
+
+
+
+exports.getuser=async(req,res)=>{
+
+    try{
+        const {name,email}=req.body
+
+       const user= await User.findOne({email})
+       if(!user){
+        throw new Error("user not be entered")
+       }
+       res.status(201).json({
+        success:true,
+        message:"User find",
+        user
+       })
+
+    }
+    catch(error){
+        console.log(error);
+        res.status(400).json({
+        success: false,
+        message: error.message,
+            
+        }) 
+    }
+}
+
+
+
+
+
+
+
+exports.deleteuser=async(req,res)=>{
+    
+    try{
+//         const {name,email}=req.body
+// const user=await User.deleteOne({})
+
+
+
+
+const userid=req.params.id;
+const user=await User.findByIdAndDelete(userid)
+
+
+if(!user){
+throw new Error("user not found")
+}
+
+
+res.status(201).json({
+    success:true,
+    message:"User delete",
+    user
+})
+
+    }
+   catch(error){
+    console.log(error)
+    res.status(400).json({
+    success: false,
+    message: error.message,
+})
+    }
+}
+
+
+
+
+
+
+exports.updateuser=async(req,res)=>{
+    try{
+const user=await User.findByIdAndUpdate(req.params.id, req.body)
+res.status(201).json({
+    success:true,
+    message:"User updated",
+    user
+})
+    }
+    catch(error){
+        console.log(error)
+        res.status(400).json({
+        success: false,
+        message: error.message,
+    })
+        }
 }
--- crud/routes/userroutes.js
+++ crud/routes/userroutes.js
@@ -1,11 +1,15 @@
 const express=require("express")
 
-const {home,createuser}=require("../controller/usercontroller.js")
+const {home,createuser,getuser,deleteuser,updateuser}=require("../controller/usercontroller.js")
 
 
 const router=express.Router()
 router.get("/",home)
 router.post('/createuser',createuser)
+router.get("/getuser",getuser)
+// router.get("/deleteuser",deleteuser)
+router.delete('/deleteuser/:id',deleteuser)
+router.put('/updateuser/:id',updateuser)
 
 
 module.exports=router
